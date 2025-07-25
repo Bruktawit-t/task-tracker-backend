7f119fbf0a842f5317b83de960881f27d49b0d61
Bruktawit <bruktawit874@gmail.com>
2025-07-25 14:23:38 -0700

    add root for render display

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 const taskRoutes = require('./src/routes/taskRoutes');
