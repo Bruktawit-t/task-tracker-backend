3dc1d49c34704460f29c9f840d9dbb1421a727cd
Bruktawit <bruktawit874@gmail.com>
2025-07-25 14:20:16 -0700

    Add root route for Render

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const cors = require('cors');
 const dotenv = require('dotenv');
 const taskRoutes = require('./src/routes/taskRoutes');
@@ -11,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
+app.get('/', (req, res) => {
+  res.send('Welcome to the Task Tracker API!');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
