f64e8f67a4383a8d259003967d1089d9097d8173
Bruktawit <bruktawit874@gmail.com>
2025-07-29 14:33:57 -0700

    host changed

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false } // Needed for remote DBs
+  
 });
 
 db.connect((err) => {
