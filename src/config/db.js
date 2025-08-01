7888df8bb7cea0985ff8cbd89b19b93bc4571929
Bruktawit <bruktawit874@gmail.com>
2025-08-01 09:41:51 -0700

    ssl fixed

--- src/config/db.js
+++ src/config/db.js
@@ -12,7 +12,10 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: false // Set false for FreeMySQLDatabase.com
+  ssl: {
+    rejectUnauthorized: true
+  }
+   // Set false for FreeMySQLDatabase.com
 });
 
 export default pool;
